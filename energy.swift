41ecd38aba2333a32dc48637459d15af2cb58458
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-07-11 00:07:34 -0400

    Update energy.swift

--- energy.swift
+++ energy.swift
@@ -2,7 +2,7 @@ let mol = (6.022*(10^23))
 let gravity = mol^2
 let mass = mol
 let Anti-gravity = gravity/(mol^2)/(-mol^2)
-let vwf = 0.0087094358027487
+let vwf = 0.87094358027487
 
 let electron = (mol*(gravity-mass)-mol*(anti-gravity+mass))^2
 let proton = (mol*(gravity+mass)+mol*(anti-gravity-mass))^2

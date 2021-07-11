b63b8363bb329de25accaa63fbbe871c6359eaac
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-07-11 00:06:32 -0400

    Create energy.swift

---
+++ energy.swift
@@ -0,0 +1,9 @@
+let mol = (6.022*(10^23))
+let gravity = mol^2
+let mass = mol
+let Anti-gravity = gravity/(mol^2)/(-mol^2)
+let vwf = 0.0087094358027487
+
+let electron = (mol*(gravity-mass)-mol*(anti-gravity+mass))^2
+let proton = (mol*(gravity+mass)+mol*(anti-gravity-mass))^2
+let neutron = (mol*(gravity+mass)*mol*(anti-gravity-mass))^2/(mol*(gravity-mass)*mol*(anti-gravity+mass))^2
